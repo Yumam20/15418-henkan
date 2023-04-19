e888065272f50f3e480700c6f011ea022b7b98fd
Joseph Wildman <josephwildman88@gmail.com>
2023-04-18 23:04:14 -0400

    develop naive implementation

---
+++ src/main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <boost/algorithm/string.hpp>
+#include "timing.h"
+
+
+using namespace std;
+
+std::map<std::string, std::string> romajiHiraganaMap;
+void read_hira(){
+    string line;
+    string word;
+    string romaji;
+    string hiragana;
+    ifstream file;
+    file.open("hiraganaMap.csv");
+    cout << "hello?" << endl;
+     if (!file.is_open())
+    {
+        cout << "path is wrong for map" << endl;
+        return;
+    }
+    while(getline(file,line)){
+        istringstream str(line);
+        cout << line << endl;
+        getline(str,word,',');//int
+        getline(str,romaji,',');//romaji
+        cout << "word " << romaji << endl;
+        getline(str,word,',');
+        getline(str,hiragana,',');//romaji
+        cout << "word " << hiragana << endl;
+        romajiHiraganaMap[romaji] = hiragana;
+    }
+ 
+    // Iterate through the map and print the elements
+    std::map<std::string, std::string>::iterator it;
+    for (it = romajiHiraganaMap.begin(); it != romajiHiraganaMap.end(); ++it);
+    {
+        cout << "Key: " << it->first << ", Value: " << it->second << std::endl;
+    }
+    cout << endl;
+}
+
+string naiveHenkan(string inputString){
+    cout << inputString << std::endl;
+    if(inputString.size() == 0){
+        return "";
+    }
+    if(inputString[0] == ' '){
+        return " " + naiveHenkan(inputString.substr(1,inputString.size()-1));
+    }
+    //need to consider cases with nobashi-bou and other typing standards resulting in smaller characters
+    if(inputString[0] == inputString[1]){
+        //first two letters are the same -> insert little tsu
+        return "っ" + naiveHenkan(inputString.substr(1,inputString.size()-1));
+    }
+    cout << "substring " << inputString << " " << endl;
+    //
+    for(size_t i = inputString.size() ; i >0 ; i--){
+        cout << "trying " << inputString.substr(0,i) << std::endl;
+
+
+        if(romajiHiraganaMap.find(inputString.substr(0,i)) != romajiHiraganaMap.end()) {
+            // mapping  found
+            cout<< "found " << romajiHiraganaMap[inputString.substr(0,i)] << " " << std::endl;
+            string recursiveCall = naiveHenkan(inputString.substr(i,inputString.size()-i));
+            cout << "searching" << inputString.substr(i,inputString.size()-i) << " " << std::endl;
+            if(true){
+                return romajiHiraganaMap[inputString.substr(0,i)] + recursiveCall;
+            }
+
+        }
+    }
+    return inputString;
+}
+
+string parallelHenkan(string inputString){
+    return "\n";
+    //pragma omp omp
+}
+
+
+int main() {
+    string inputString;
+    string outputString;
+    cout << "Please Enter the Input" << std::endl;
+    getline(cin, inputString);
+    boost::algorithm::to_lower(inputString);
+    read_hira();
+    //insert timing code
+    Timer totalSimulationTimer; 
+    string naiveOutput = naiveHenkan(inputString);
+    double totalSimulationTime = totalSimulationTimer.elapsed(); 
+    printf("total simulation time: %.6fs\n", totalSimulationTime); 
+    //insert timing code
+    string parallelOutput = parallelHenkan(inputString);
+    //insert timing code
+    cout << "naiveHenkan: " << naiveOutput << std::endl;
+    cout << "parallelHenkan: " << parallelOutput << std::endl;
+    return 0;
+}
+
+// eigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimasen eigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimaseneigo wakarimasen eigo wakarimasen Ajia Amerikajin eigo wakarimasen
+
