228792605175aac0cfc889f6f0d1447bae651781
Yumam20 <yumam@andrew.cmu.edu>
2023-05-02 00:57:22 -0400

    not working testHenkan,more readability for dict query

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ void read_hira(){
     string hiragana;
     ifstream file;
     file.open("hiraganaMap.csv");
-    cout << "hello?" << endl;
+    //cout << "hello?" << endl;
      if (!file.is_open())
     {
         cout << "path is wrong for map" << endl;
@@ -26,27 +26,30 @@ void read_hira(){
     }
     while(getline(file,line)){
         istringstream str(line);
-        cout << line << endl;
+        //cout << line << endl;
         getline(str,word,',');//int
         getline(str,romaji,',');//romaji
-        cout << "word " << romaji << endl;
+        //cout << "word " << romaji << endl;
         getline(str,word,',');
         getline(str,hiragana,',');//romaji
-        cout << "word " << hiragana << endl;
+        //cout << "word " << hiragana << endl;
         romajiHiraganaMap[romaji] = hiragana;
     }
- 
+
     // Iterate through the map and print the elements
     std::map<std::string, std::string>::iterator it;
     for (it = romajiHiraganaMap.begin(); it != romajiHiraganaMap.end(); ++it);
     {
-        cout << "Key: " << it->first << ", Value: " << it->second << std::endl;
+        //cout << "Key: " << it->first << ", Value: " << it->second << std::endl;
     }
     cout << endl;
 }
+bool inDict(string queryString){
+    return romajiHiraganaMap.find(queryString) != romajiHiraganaMap.end();
+}
 
 string naiveHenkan(string inputString){
-    cout << inputString << std::endl;
+    //cout << inputString << std::endl;
     if(inputString.size() == 0){
         return "";
     }
@@ -58,17 +61,17 @@ string naiveHenkan(string inputString){
         //first two letters are the same -> insert little tsu
         return "っ" + naiveHenkan(inputString.substr(1,inputString.size()-1));
     }
-    cout << "substring " << inputString << " " << endl;
+    //cout << "substring " << inputString << " " << endl;
     //
     for(size_t i = inputString.size() ; i >0 ; i--){
-        cout << "trying " << inputString.substr(0,i) << std::endl;
+        //cout << "trying " << inputString.substr(0,i) << std::endl;
 
 
-        if(romajiHiraganaMap.find(inputString.substr(0,i)) != romajiHiraganaMap.end()) {
+        if(inDict(inputString.substr(0,i))) {
             // mapping  found
-            cout<< "found " << romajiHiraganaMap[inputString.substr(0,i)] << " " << std::endl;
+            //cout<< "found " << romajiHiraganaMap[inputString.substr(0,i)] << " " << std::endl;
             string recursiveCall = naiveHenkan(inputString.substr(i,inputString.size()-i));
-            cout << "searching" << inputString.substr(i,inputString.size()-i) << " " << std::endl;
+            //cout << "searching" << inputString.substr(i,inputString.size()-i) << " " << std::endl;
             if(true){
                 return romajiHiraganaMap[inputString.substr(0,i)] + recursiveCall;
             }
@@ -78,10 +81,44 @@ string naiveHenkan(string inputString){
     return inputString;
 }
 
+string testHenkan(string inputString){
+    //int middle = inputString.size() / 2;
+    int frontBound, endBound;
+    frontBound = 0; endBound = inputString.size();
+    string returnMe = "";
+
+    while(frontBound < endBound){
+        for(int i = 3; i > 0; i++){ //3 is maxsize romaji in dict
+
+            if(inDict(inputString.substr(frontBound,i))){
+                returnMe.insert(0,romajiHiraganaMap[inputString.substr(frontBound,i)]);
+                frontBound += i;
+            }
+
+            if(inDict(inputString.substr(endBound-i,i))){
+                returnMe += romajiHiraganaMap[inputString.substr(frontBound,i)];
+                endBound -= i;
+            }
+        } //might want to break up for loops to ensure that 1 is not stall waiting for other if already found
+    }
+    return returnMe;
+
+}
+/*
 string parallelHenkan(string inputString){
+    for(size_t chunk = 3; chunk >= 0; chunk--){
+        #pragma omp parallel
+        {
+            for(int i = 0 ; i < (inputString.size() / chunk) ; i += chunk){
+                if(romajiHiraganaMap.find(inputString.substr(i,chunk)) != romajiHiraganaMap.end()) {
+                
+                }
+            } 
+        }
+    }
     return "\n";
     //pragma omp omp
-}
+}*/
 
 
 int main() {
@@ -92,15 +129,18 @@ int main() {
     boost::algorithm::to_lower(inputString);
     read_hira();
     //insert timing code
-    Timer totalSimulationTimer; 
+    Timer totalSimulationTimer;
     string naiveOutput = naiveHenkan(inputString);
-    double totalSimulationTime = totalSimulationTimer.elapsed(); 
-    printf("total simulation time: %.6fs\n", totalSimulationTime); 
+    double totalSimulationTime = totalSimulationTimer.elapsed();
+    printf("total simulation time: %.6fs\n", totalSimulationTime);
     //insert timing code
-    string parallelOutput = parallelHenkan(inputString);
+    //string parallelOutput = parallelHenkan(inputString);
+
+    string testOutput = testHenkan(inputString);
     //insert timing code
     cout << "naiveHenkan: " << naiveOutput << std::endl;
-    cout << "parallelHenkan: " << parallelOutput << std::endl;
+    //cout << "parallelHenkan: " << parallelOutput << std::endl;
+    cout << "testHenkan: " << testOutput << std::endl;
     return 0;
 }
 
